a5c0a7b6e5cf412b781d7ac186942020746ef497
Camas <camas@users.noreply.github.com>
2020-12-10 01:07:36 +0000

    Change loops to iterators

--- src/y2020/day9.rs
+++ src/y2020/day9.rs
@@ -4,41 +4,50 @@ pub fn solve(input: &str) -> (impl ToString, impl ToString) {
         .map(|line| line.parse::<i64>().unwrap())
         .collect::<Vec<_>>();
 
-    let part1_index = (25..input.len())
-        .find(|&i| {
+    let part1 = (25..input.len())
+        .find_map(|i| {
             let num = input[i];
-            let mut found = false;
-            'outer: for (u, &a) in input.iter().skip(i - 25).take(25).enumerate() {
-                for (v, &b) in input.iter().skip(i - 25).take(25).enumerate() {
-                    if u == v {
-                        continue;
-                    }
-                    if a + b == num {
-                        found = true;
-                        break 'outer;
-                    }
-                }
+            let has_sum = input
+                .iter()
+                .skip(i - 25)
+                .take(25)
+                .enumerate()
+                .any(|(u, &a)| {
+                    input
+                        .iter()
+                        .skip(i - 25)
+                        .take(25)
+                        .enumerate()
+                        .any(|(v, &b)| u != v && a + b == num)
+                });
+            if !has_sum {
+                Some(num)
+            } else {
+                None
             }
-            !found
         })
         .unwrap();
-    let part1 = input[part1_index];
 
-    let mut part2 = 0;
-    'outer: for i in 0..input.len() {
-        let mut sum = 0;
-        for (j, &value) in input[i..].iter().enumerate() {
-            sum += value;
+    let part2 = (0..input.len())
+        .find_map(|i| {
+            let mut sum = 0;
+            let j = input[i..]
+                .iter()
+                .enumerate()
+                .find(|(_, &n)| {
+                    sum += n;
+                    sum >= part1
+                })
+                .unwrap()
+                .0;
             if sum == part1 {
                 let range = &input[i..(i + j)];
-                part2 = range.iter().max().unwrap() + range.iter().min().unwrap();
-                break 'outer;
+                Some(range.iter().max().unwrap() + range.iter().min().unwrap())
+            } else {
+                None
             }
-            if sum > part1 {
-                continue 'outer;
-            }
-        }
-    }
+        })
+        .unwrap();
 
     (part1, part2)
 }
