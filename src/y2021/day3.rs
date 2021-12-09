5f3780099c7fbaece5c9331de5cc8cd3f987404c
camas <camas@users.noreply.github.com>
2021-12-09 18:34:19 +0000

    2020 day 3

--- src/y2021/day3.rs
+++ src/y2021/day3.rs
@@ -1,3 +1,5 @@
+use std::cmp::Ordering;
+
 pub fn solve(input: &str) -> (impl ToString, impl ToString) {
     let numbers = input
         .lines()
@@ -7,30 +9,9 @@ pub fn solve(input: &str) -> (impl ToString, impl ToString) {
                 .collect::<Vec<_>>()
         })
         .collect::<Vec<_>>();
+    let inner_len = numbers[0].len();
 
-    // Count the number of set bits for each position
-    let mut zero_counts = vec![0; numbers[0].len()];
-    let mut one_counts = vec![0; numbers[0].len()];
-    for n in numbers.iter() {
-        for (i, &v) in n.iter().enumerate() {
-            if v == 1 {
-                one_counts[i] += 1;
-            } else {
-                zero_counts[i] += 1;
-            }
-        }
-    }
-
-    let gamma = one_counts
-        .iter()
-        .map(|c| {
-            if *c > (numbers.len() as u32 / 2) {
-                1
-            } else {
-                0
-            }
-        })
-        .collect::<Vec<_>>();
+    let gamma = most_common_bits(&numbers);
     let epsilon = gamma
         .iter()
         .map(|v| if *v == 1 { 0 } else { 1 })
@@ -39,43 +20,55 @@ pub fn solve(input: &str) -> (impl ToString, impl ToString) {
     // Part 1 = most common (gamma) * least common (epsilon)
     let part1 = bits_to_int(&gamma) * bits_to_int(&epsilon);
 
-    let most_common = one_counts
-        .iter()
-        .zip(zero_counts.iter())
-        .map(|(o, z)| if o >= z { 1 } else { 0 })
-        .collect::<Vec<_>>();
     let mut search_space = numbers.clone();
-    let mut position = 0;
-    while search_space.len() > 1 {
-        let most_common = most_common[position];
-        search_space = search_space
-            .into_iter()
-            .filter(|n| n[position] == most_common)
-            .collect();
-        position += 1;
+    for i in 0..inner_len {
+        let most_common = most_common_bits(&search_space);
+        search_space.retain(|v| v[i] == most_common[i]);
+        if search_space.len() == 1 {
+            break;
+        }
     }
     let oxygen_rating = bits_to_int(&search_space[0]);
 
-    let least_common = one_counts
-        .iter()
-        .zip(zero_counts.iter())
-        .map(|(o, z)| if z <= o { 0 } else { 1 })
-        .collect::<Vec<_>>();
     let mut search_space = numbers;
-    let mut position = 0;
-    while search_space.len() > 1 {
-        let least_common = least_common[position];
-        search_space = search_space
+    for i in 0..inner_len {
+        let most_common = most_common_bits(&search_space);
+        let least_common = most_common
             .into_iter()
-            .filter(|n| n[position] == least_common)
-            .collect();
-        position += 1;
+            .map(|v| if v == 1 { 0 } else { 1 })
+            .collect::<Vec<_>>();
+        search_space.retain(|v| v[i] == least_common[i]);
+        if search_space.len() == 1 {
+            break;
+        }
     }
     let co2_rating = bits_to_int(&search_space[0]);
 
     let part2 = oxygen_rating * co2_rating;
 
     (part1, part2)
+}
+
+fn most_common_bits(numbers: &[Vec<u32>]) -> Vec<u32> {
+    // Count the number of each bit in each position
+    let mut counts = vec![(0, 0); numbers[0].len()];
+    for n in numbers.iter() {
+        for (i, &v) in n.iter().enumerate() {
+            if v == 0 {
+                counts[i].0 += 1;
+            } else {
+                counts[i].1 += 1;
+            }
+        }
+    }
+    // Return the most common bit, or 1 if there is a tie
+    counts
+        .into_iter()
+        .map(|(zeros, ones)| match zeros.cmp(&ones) {
+            Ordering::Less | Ordering::Equal => 1,
+            Ordering::Greater => 0,
+        })
+        .collect::<Vec<_>>()
 }
 
 fn bits_to_int(bits: &[u32]) -> u32 {
